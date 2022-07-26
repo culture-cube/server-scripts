b3eae4ebb5f6b21268df4d34f1a58b9a46539575
Greg Lorenzen <greg.lorenzen@goculturecube.com>
2022-07-26 11:59:06 -0700

    Removed check from udpate-sites.sh script.

--- update-sites.sh
+++ update-sites.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
-# Confirm before running plugin updates
-read -r -p "Are you sure you want to update all specified directories? [y/N]" RESPONSE
+BACKUPPATH=~/backups
 
-	if [[ "$RESPONSE" =~ ^([yY][eE][sS]|[yY]) ]]; then
+cd
+ROOT="$(pwd)"
 
-		BACKUPPATH=~/backups
+# For each site, run updates and backups
+for SITE in $(ls $ROOT); do
+	if [ -d $ROOT/$SITE/public ]; then
+		echo "Processing $SITE..."
+		cd $ROOT/$SITE/public
 
-		cd
-		ROOT="$(pwd)"
+		# First backup the site
+		echo "Backing up $SITE..."
+		if [ -f "$SITE.sql" ]; then rm $SITE.sql; fi
+		echo "Exporting database..."
+		wp db export $SITE.sql
+		if [ -f "$BACKUPPATH/$SITE.tar.gz" ]; then rm $BACKUPPATH/$SITE.tar.gz; fi
+		echo "Backing up files..."
+		tar czf $BACKUPPATH/$SITE.tar.gz $SITE.sql ./wp-content/plugins ./wp-content/themes ./wp-content/uploads
+		rm $SITE.sql;
 
-		# For each site, run updates and backups
-		for SITE in $(ls $ROOT); do
-			if [ -d $ROOT/$SITE/public ]; then
-				echo "Processing $SITE..."
-				cd $ROOT/$SITE/public
-
-				# First backup the site
-				echo "Backing up $SITE..."
-				if [ -f "$SITE.sql" ]; then rm $SITE.sql; fi
-				echo "Exporting database..."
-				wp db export $SITE.sql
-				if [ -f "$BACKUPPATH/$SITE.tar.gz" ]; then rm $BACKUPPATH/$SITE.tar.gz; fi
-				echo "Backing up files..."
-				tar czf $BACKUPPATH/$SITE.tar.gz $SITE.sql ./wp-content/plugins ./wp-content/themes ./wp-content/uploads
-				rm $SITE.sql;
-
-				# Run updates
-				wp theme update --all;
-				wp plugin update --all --minor;
-				wp theme list;
-				wp plugin list;
-				wp core update;
-				wp checksum core;
-				wp db check;
-				echo ""
-			fi
-		done
-
+		# Run updates
+		wp theme update --all;
+		wp plugin update --all --minor;
+		wp theme list;
+		wp plugin list;
+		wp core update;
+		wp checksum core;
+		wp db check;
+		echo ""
 	fi
+done
